4b88bc98ad2d05c38c52fd260df6095b54d861f0
cigamit <jimmy@sqmail.org>
2018-01-16 05:36:44 -0600

    Not actually entirely fixed.  There were more entries

--- Text/Diff.php
+++ Text/Diff.php
@@ -326,7 +326,7 @@ class Text_Diff_Op_copy extends Text_Diff_Op {
         $this->final = $final;
     }
 
-    function reverse() {
+    function &reverse() {
         $reverse = new Text_Diff_Op_copy($this->final, $this->orig);
         return $reverse;
     }
@@ -347,7 +347,7 @@ class Text_Diff_Op_delete extends Text_Diff_Op {
     }
 
     function &reverse() {
-        $reverse = &new Text_Diff_Op_add($this->orig);
+        $reverse = new Text_Diff_Op_add($this->orig);
         return $reverse;
     }
 
@@ -367,7 +367,7 @@ class Text_Diff_Op_add extends Text_Diff_Op {
     }
 
     function &reverse() {
-        $reverse = &new Text_Diff_Op_delete($this->final);
+        $reverse = new Text_Diff_Op_delete($this->final);
         return $reverse;
     }
 
@@ -387,7 +387,7 @@ class Text_Diff_Op_change extends Text_Diff_Op {
     }
 
     function &reverse() {
-        $reverse = &new Text_Diff_Op_change($this->final, $this->orig);
+        $reverse = new Text_Diff_Op_change($this->final, $this->orig);
         return $reverse;
     }
 
