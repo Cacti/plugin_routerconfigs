df0215f0977a854e65f508ec99b4a7f4c3ed7df8
netniV <netniv@hotmail.com>
2018-02-18 22:08:40 +0000

    Remove index.php from being included by HordeTextInclude.php

--- HordeTextInclude.php
+++ HordeTextInclude.php
@@ -8,8 +8,6 @@ include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Inline.p
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Unified.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Context.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Unified/Colored.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Unified/index.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/index.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Delete.php
@@ -17,14 +15,10 @@ include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Change.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Copy.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Base.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Add.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/index.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/xdiff.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/string.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/native.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/shell.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/index.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/index.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Exception.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff/Mapped.php
 include_once(['base_path'] . '/plugins/routerconfigs/Text/Diff.php
-include_once(['base_path'] . '/plugins/routerconfigs/Text/index.php
