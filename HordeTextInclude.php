5e0e3d03f29f04d34582e3be2ba2cfd086670a0c
netniV <netniv@hotmail.com>
2018-02-18 22:17:19 +0000

    A little reorg to get things loading in the correct order

--- HordeTextInclude.php
+++ HordeTextInclude.php
@@ -1,19 +1,18 @@
 <?php
+include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay/BlockBuilder.php');
-include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay/Op/Copy.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay/Op/Base.php');
-include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay/index.php');
+include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay/Op/Copy.php');
+include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/table.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Inline.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Unified.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Context.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer/Unified/Colored.php');
-include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/ThreeWay.php');
-include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Renderer.php');
+include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Base.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Delete.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Change.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Copy.php');
-include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Base.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Op/Add.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/xdiff.php');
 include_once($config['base_path'] . '/plugins/routerconfigs/Text/Diff/Engine/string.php');
